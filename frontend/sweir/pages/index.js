0657ee86d5188d7fda2701e0298fc5010dd6403c
Emmanuel <esackey15@alustudent.com>
2021-03-02 11:29:48 +0000

    Setup Backend and Delete Next JS Boilerplate Code

---
+++ frontend/sweir/pages/index.js
@@ -0,0 +1,22 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div className="container">
+      <Head>
+        <title>Software Engineer in Residence</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+       
+      </main>
+
+      <footer>
+        <p> Copyright Immanuel for SWEIR &copy; 2021</p>
+      </footer>
+
+
+    </div>
+  )
+}
