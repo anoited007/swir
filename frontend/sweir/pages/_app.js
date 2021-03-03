df256ac0e09fa65214b44cf93bfa4ca5ab681a68
Emmanuel <esackey15@alustudent.com>
2021-03-03 14:58:03 +0000

    Manual form validation interfering with redirect
    Using class component to perform manual validation is interfering with next js router push function  which only works in functional components.

--- frontend/sweir/pages/_app.js
+++ frontend/sweir/pages/_app.js
@@ -1,4 +1,6 @@
 import styles from '../styles/global.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 export default function App({ Component, pageProps }) {
     return <Component {...pageProps} />
---
+++ frontend/sweir/shared/sharedConstants.js
@@ -0,0 +1,3 @@
+export const localBackend = "http://localhost:8000/auth/";
+export const remoteBackend = "";
+export const remote = false;
